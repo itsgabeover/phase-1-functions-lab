be1291fe9315d5673dd9babaff8e56541ad53b35
itsgabeover <gabe.enrique.miranda@gmail.com>
2021-06-28 15:51:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    let blocks = Math.abs(pickUpLocation - 42);
+    return blocks;
+}
+
+function distanceFromHqInFeet(someValue){
+    let feet = 264 * distanceFromHqInBlocks(someValue);
+    return feet;
+}
+
+function  distanceTravelledInFeet(start, end){
+    let distanceInFeet = Math.abs(start - end)*264;
+    return distanceInFeet;
+}
+
+function calculatesFarePrice(start, end){
+    let distanceInFeet = Math.abs(start - end)*264;
+    if (distanceInFeet > 2500) {
+        return 'cannot travel that far';
+    } else if (distanceInFeet > 2000){
+        return 25;
+    }
+    else if (distanceInFeet < 400){
+        return 0;
+    }
+    else {
+        return (distanceInFeet - 400)*.02;
+    }
+}
